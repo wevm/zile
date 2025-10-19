54852144a55dfbe604ba5859e1bda693f031272b
jxom <7336481+jxom@users.noreply.github.com>
2025-10-19 17:40:21 +1100

    chore: tweaks

--- src/Package.ts
+++ src/Package.ts
@@ -500,7 +500,7 @@ export async function transpile(options: transpile.Options): Promise<transpile.R
   const tsConfig = {
     compilerOptions,
     exclude: tsConfigJson.exclude ?? [],
-    include: entries,
+    include: [...(tsConfigJson.include ?? []), ...entries],
   } as const
 
   const tmpProject = path.resolve(cwd, 'tsconfig.tmp.json')
